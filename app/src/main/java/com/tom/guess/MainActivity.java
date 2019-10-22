d720bbda7cb15002d124c3ed43a0c2346503fd5c
miia <miia@com.tw>
2019-10-22 16:29:30 +0800

    second

--- app/src/main/java/com/tom/guess/MainActivity.java
+++ app/src/main/java/com/tom/guess/MainActivity.java
@@ -8,11 +8,22 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    int secret = new Random().nextInt(10)+1;
+    private EditText number;
+    String TAG = MainActivity.class.getSimpleName();
+    private TextView hint;
+    private ImageView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +31,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        number = findViewById(R.id.num);
+        hint = findViewById(R.id.hint);
+        result = findViewById(R.id.result);
 
+        Log.d(TAG,"SECRET:"+secret);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,7 +45,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    public void guess(View view){
+        int guessnum = Integer.parseInt(number.getText().toString());
+        result.setVisibility(View.VISIBLE);
+        if (guessnum > secret){
+            hint.setText("smaller");
+            result.setImageResource(R.drawable.down);
+        }else if (guessnum < secret){
+            hint.setText("bigger");
+            result.setImageResource(R.drawable.up);
+        }else {
+            hint.setText("bingo, the secret number is:" + secret);
+            result.setImageResource(R.drawable.flower);
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
