e13478741a75278f2b6be4a2f6096762cfd39d61
miia <miia@com.tw>
2019-10-29 16:28:46 +0800

    new

--- app/src/main/java/com/tom/guess/MainActivity.java
+++ app/src/main/java/com/tom/guess/MainActivity.java
@@ -1,10 +1,12 @@
 package com.tom.guess;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -12,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,13 +22,16 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    int secret = new Random().nextInt(10)+1;
+    //int secret = new Random().nextInt(10)+1;
+    //int c = 5;
+    int secret;
     private EditText number;
     String TAG = MainActivity.class.getSimpleName();
     private TextView hint;
     private ImageView result;
-    int c = 5;
     private TextView time;
+    int counter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,36 +43,73 @@ public class MainActivity extends AppCompatActivity {
         hint = findViewById(R.id.hint);
         result = findViewById(R.id.result);
         time = findViewById(R.id.time);
-
-        Log.d(TAG,"SECRET:"+secret);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               reset();
             }
         });
+        reset();
+        Log.d(TAG,"SECRET:"+secret);
     }
+
+    public void reset(){
+        secret = new Random().nextInt(10)+1;
+        counter = 0;
+        time.setText(counter+"");
+    }
+
     public void guess(View view){
-        c = c-1;
+        //c = c-1;
         int guessnum = Integer.parseInt(number.getText().toString());
         result.setVisibility(View.VISIBLE);
+        counter++;
+        time.setText(counter+"");
+        DialogInterface.OnCancelListener listener = new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                reset();
+            }
+        };
+        String message = "good";
         if (guessnum > secret){
             hint.setText("smaller");
             result.setImageResource(R.drawable.down);
+            message = "smaller";
+            //listener = null;
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Hint")
+                    .setMessage("smaller" )
+                    .setPositiveButton("ok",null)
+                    .show();
         }else if (guessnum < secret){
             hint.setText("bigger");
             result.setImageResource(R.drawable.up);
+            message = "bigger";
+            //listener = null;
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Hint")
+                    .setMessage("smaller" )
+                    .setPositiveButton("ok",null)
+                    .show();
         }else {
             hint.setText("bingo, the secret number is:" + secret);
             result.setImageResource(R.drawable.flower);
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Hint")
+                    .setMessage("bingo" )
+                    .setPositiveButton("ok",null)
+                    .show();
         }
-        if (c > 5 || c <= 0){
-            time.setText("game over");
-        }else {
+
+
+        /*
+        if (c > 0 && c < 5){
             time.setText("remaining times:" + c);
-        }
+        }else if (c < 0) {
+            time.setText("game over");
+        }*/
     }
 
     @Override
